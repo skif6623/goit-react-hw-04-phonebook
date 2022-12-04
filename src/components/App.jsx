7f96287ebf88f8625d5c4313337a9bd5bea98b6f
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-04 13:14:48 +0200

    completed the first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template бубу
-    </div>
-  );
-};
+import React, { Component } from 'react';
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onButtonClick = e => {
+    const { name } = e.currentTarget;
+
+    this.setState(prevState => {
+      return {
+        [name]: prevState[name] + 1,
+      };
+    });
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <div>
+        <h1>Pleade leave feedback</h1>
+        <button name="good" type="button" onClick={this.onButtonClick}>
+          Good
+        </button>
+        <button name="neutral" type="button" onClick={this.onButtonClick}>
+          Neutral
+        </button>
+        <button name="bad" type="button" onClick={this.onButtonClick}>
+          Bad
+        </button>
+        <p>Statistics</p>
+        <ul>
+          <li>
+            <p>Good: {good}</p>
+          </li>
+          <li>
+            <p>Neutral: {neutral}</p>{' '}
+          </li>
+          <li>
+            <p>Bad: {bad}</p>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
