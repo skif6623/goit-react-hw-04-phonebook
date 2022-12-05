e0fe2ec5470d94ec41d886d1ed821080993b38f8
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-05 17:21:34 +0200

    added filter description

--- src/components/ContactsFilter/ContactsFilter.jsx
+++ src/components/ContactsFilter/ContactsFilter.jsx
@@ -1,6 +1,7 @@
 export const ContactsFilter = ({ filter, changeFilter }) => {
   return (
     <label>
+      Find contacts by name
       <input type="text" value={filter} onChange={changeFilter} />
     </label>
   );
