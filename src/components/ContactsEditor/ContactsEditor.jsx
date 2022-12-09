4cd233b1253e5665dd5a08d52bcc7277841ea1cf
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-09 15:40:17 +0200

    add formik to contactEditor

--- src/components/ContactsEditor/ContactsEditor.jsx
+++ src/components/ContactsEditor/ContactsEditor.jsx
@@ -8,78 +8,44 @@ import {
   FormBtn,
 } from './ContactsEditor.styled';
 import { Box } from 'components/Box';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-export class ContactsEditor extends Component {
-  static propTypes = {
-    title: PropTypes.string,
-    addContact: PropTypes.func.isRequired,
+const initialValues = {
+  name: '',
+  number: '',
+};
+
+export const ContactsEditor = ({ title, addContact }) => {
+  const handleSubmit = (values, { resetForm }) => {
+    console.log(values);
+    addContact(values);
+
+    resetForm();
   };
 
-  state = {
-    name: '',
-    number: '',
-  };
-
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
-    const { addContact } = this.props;
-
-    e.preventDefault();
-
-    addContact(this.state);
-
-    this.resetName();
-  };
-
-  resetName = () => {
-    this.setState({ name: '', number: '' });
-  };
-
-  render() {
-    const { name, number } = this.state;
-    const { title } = this.props;
-
-    return (
-      <>
-        {title && <ContactsTitle>{title}</ContactsTitle>}
-
-        <AddForm onSubmit={this.handleSubmit}>
+  return (
+    <>
+      {title && <ContactsTitle>{title}</ContactsTitle>}
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form>
           <Box mb={10}>
             <FormLabel htmlFor="name">Name</FormLabel>
-            <FormInput
-              id="name"
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              value={name}
-              onChange={this.handleChange}
-            />
+            <Field id="name" type="text" name="name" />
           </Box>
 
           <Box mb={25}>
             <FormLabel htmlFor="number">Number</FormLabel>
-            <FormInput
-              id="number"
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              value={number}
-              onChange={this.handleChange}
-              required
-            />
+            <Field id="number" type="tel" name="number" />
           </Box>
 
           <FormBtn type="submit">Add Contact</FormBtn>
-        </AddForm>
-      </>
-    );
-  }
-}
+        </Form>
+      </Formik>
+    </>
+  );
+};
+
+ContactsEditor.propTypes = {
+  title: PropTypes.string,
+  addContact: PropTypes.func.isRequired,
+};
