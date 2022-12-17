869deef2920cf4fd77123a8c4d7d4b0a7b257bc1
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-17 23:59:22 +0200

    added use state to app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import {useState} from 'react';
 import { nanoid } from 'nanoid';
 import { ContactsEditor } from '../ContactsEditor/ContactsEditor';
 import { ContactsList } from '../ContactsList/ContactsList';
@@ -6,15 +6,14 @@ import { ContactsFilter } from '../ContactsFilter/ContactsFilter';
 import { GlobalStyle } from '../GlobalStyle';
 import { BookApp } from './App.styled';
 
-export class App extends Component {
-  state = {
-    contacts: [
+export const App () => {
+  const [contacts, setContacts] = useState([
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+  ])
+  const [filter, setFilter] = useState('')
   };
 
   componentDidMount() {
@@ -93,3 +92,91 @@ export class App extends Component {
     );
   }
 }
+
+// export class App extends Component {
+//   state = {
+//     contacts: [
+//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//     ],
+//     filter: '',
+//   };
+
+//   componentDidMount() {
+//     const initContacts = localStorage.getItem('contacts');
+
+//     if (initContacts) {
+//       this.setState({
+//         contacts: JSON.parse(initContacts),
+//       });
+//     }
+//   }
+
+//   componentDidUpdate(_, prevState) {
+//     if (prevState.contacts !== this.state.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+//   addContact = ({ name, number }) => {
+//     const { contacts } = this.state;
+//     const duplicateContact = contacts.find(contact => contact.name === name);
+//     const contact = {
+//       name,
+//       number,
+//       id: nanoid(),
+//     };
+
+//     duplicateContact !== undefined
+//       ? alert(`${name} is alredy in contacts.`)
+//       : this.setState(({ contacts }) => ({
+//           contacts: [contact, ...contacts],
+//         }));
+//   };
+
+//   changeFilter = e => {
+//     const { value } = e.target;
+
+//     this.setState({
+//       filter: value,
+//     });
+//   };
+
+//   getVisibleContacts = () => {
+//     const { contacts, filter } = this.state;
+//     const normalizedFilter = filter.toLowerCase();
+
+//     return contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(normalizedFilter)
+//     );
+//   };
+
+//   deleteContact = contactId => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+//     }));
+//   };
+
+//   render() {
+//     const { filter } = this.state;
+//     const visibleContacts = this.getVisibleContacts();
+
+//     return (
+//       <BookApp>
+//         <ContactsEditor title="Phonebook" addContact={this.addContact} />
+//         <ContactsFilter
+//           title="Contacts"
+//           filter={filter}
+//           changeFilter={this.changeFilter}
+//         />
+//         <ContactsList
+//           contacts={visibleContacts}
+//           deleteContact={this.deleteContact}
+//         />
+//         <GlobalStyle />
+//       </BookApp>
+//     );
+//   }
+// }
