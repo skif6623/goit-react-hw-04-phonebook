72db40fb76dc2ec1d4b4eb9d18c49b45b8c0c0f1
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-19 18:58:41 +0200

    added set in local storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactsEditor } from '../ContactsEditor/ContactsEditor';
 import { ContactsList } from '../ContactsList/ContactsList';
@@ -6,14 +6,20 @@ import { ContactsFilter } from '../ContactsFilter/ContactsFilter';
 import { GlobalStyle } from '../GlobalStyle';
 import { BookApp } from './App.styled';
 
+const contactsIntoBook = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(contactsIntoBook);
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const addContact = ({ name, number }) => {
     const duplicateContact = contacts.find(contact => contact.name === name);
@@ -48,8 +54,6 @@ export const App = () => {
     );
   };
 
-  const visibleContacts = getVisibleContacts();
-
   return (
     <BookApp>
       <ContactsEditor title="Phonebook" addContact={addContact} />
@@ -58,7 +62,10 @@ export const App = () => {
         filter={filter}
         changeFilter={changeFilter}
       />
-      <ContactsList contacts={visibleContacts} deleteContact={deleteContact} />
+      <ContactsList
+        contacts={getVisibleContacts()}
+        deleteContact={deleteContact}
+      />
       <GlobalStyle />
     </BookApp>
   );
