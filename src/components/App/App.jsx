881f9bcbab6175996b7d7b96a485b4fe6e67c6cf
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-20 00:05:13 +0200

    added get contacts from LS

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,9 +12,12 @@ const contactsIntoBook = [
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
+const savedStorageContacts = JSON.parse(localStorage.getItem('contacts'));
 
 export const App = () => {
-  const [contacts, setContacts] = useState(contactsIntoBook);
+  const [contacts, setContacts] = useState(
+    savedStorageContacts || contactsIntoBook
+  );
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
@@ -23,6 +26,7 @@ export const App = () => {
 
   const addContact = ({ name, number }) => {
     const duplicateContact = contacts.find(contact => contact.name === name);
+    console.log(duplicateContact);
     const newContact = {
       name,
       number,
