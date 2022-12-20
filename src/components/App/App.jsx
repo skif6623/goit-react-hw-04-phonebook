f24852131a21be72d796c4ac695869ee96e7e159
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-12-20 22:21:14 +0200

    change parse data frol LS

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,12 +12,16 @@ const contactsIntoBook = [
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
-const savedStorageContacts = JSON.parse(localStorage.getItem('contacts'));
+const getContactsFromStorage = () => {
+  const savedStorageContacts = localStorage.getItem('contacts');
+  if (savedStorageContacts !== null) {
+    return JSON.parse(savedStorageContacts);
+  }
+  return contactsIntoBook;
+};
 
 export const App = () => {
-  const [contacts, setContacts] = useState(
-    savedStorageContacts || contactsIntoBook
-  );
+  const [contacts, setContacts] = useState(getContactsFromStorage());
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
